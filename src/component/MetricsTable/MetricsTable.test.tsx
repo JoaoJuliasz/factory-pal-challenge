f8e4c8a5924984e14d24e15d45e81f11b1c4bb2e
JoaoJuliasz <joaojuliaszdemorais@gmail.com>
2024-06-06 15:01:15 -0300

    replace vitest by jest

--- src/component/MetricsTable/MetricsTable.test.tsx
+++ src/component/MetricsTable/MetricsTable.test.tsx
@@ -1,4 +1,3 @@
-import { describe, expect, it, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import MetricsTable from './MetricsTable'
 
--- src/component/TypeButton/TypeButton.test.tsx
+++ src/component/TypeButton/TypeButton.test.tsx
@@ -1,16 +1,16 @@
-import { describe, expect, it, vi } from 'vitest'
+// import { describe, expect, it, vi } from 'vitest'
 import { fireEvent, render, screen } from '@testing-library/react'
 import TypeButton from './TypeButton'
 
 describe('TypeButton', () => {
     it('should render button', () => {
-        render(<TypeButton title="test title" selectedType="all" setSelectedMetricType={vi.fn()} />)
+        render(<TypeButton title="test title" selectedType="all" setSelectedMetricType={jest.fn()} />)
         const title = screen.getByText('test title')
         expect(title).toBeInTheDocument()
     })
 
-    it('should call setSelectedMetricType', () => {
-        const setSelectedMetricType = vi.fn()
+    it('should trigger setSelectedMetricType on button click', () => {
+        const setSelectedMetricType = jest.fn()
         render(<TypeButton title="test title" selectedType="all" setSelectedMetricType={setSelectedMetricType} />)
         const title = screen.getByText('test title')
         fireEvent.click(title)
